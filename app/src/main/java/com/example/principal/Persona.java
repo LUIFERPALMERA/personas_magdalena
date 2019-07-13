e6cc419863aacb5ea575253bdd77878894d34881
Luis Fernando <luiferpalmera@gmail.com>
2019-07-12 21:04:17 -0500

    Creacion clase persona

---
+++ app/src/main/java/com/example/principal/Persona.java
@@ -0,0 +1,37 @@
+package com.example.principal;
+
+public class Persona {
+    private String cedula;
+    private String nombres;
+    private String apellidos;
+
+    public Persona(String cedula, String nombres, String apellidos) {
+        this.cedula = cedula;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+}
